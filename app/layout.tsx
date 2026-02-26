22fd4331d85a45cf4dd6bfb3ec13f1849e288fa7
Nishant Sharma <197467920+Nishant-444@users.noreply.github.com>
2026-02-26 17:33:21 +0530

    feat: add Toaster component for improved user notifications in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/navbar';
 import { Suspense } from 'react';
+import { Toaster } from '@/components/ui/sonner';
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -46,6 +47,7 @@ export default function RootLayout({
 				<Suspense fallback={<NavbarSkeleton />}>
 					<Navbar />
 				</Suspense>
+				<Toaster position='bottom-right' />
 				{children}
 			</body>
 		</html>
